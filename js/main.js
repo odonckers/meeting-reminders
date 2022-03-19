62904d9aefce6b42144729c986d5371707d5b6e0
gevoghaj <41239356+gevoghaj@users.noreply.github.com>
2022-03-19 08:51:37 -0500

    added ability for user to choose meeting and set timer accordingly.
    - Added start page where user chooses between Sunday and Thursday
    - Sunday and Thursday buttons are linked to a changeVal function in main.js that sets variables h,m, and umgap that change the values of hour, minute, and mGap respectively
    - addded CSS for divs in start page and buttons
    - added div ids to sections of clock so that they can be hidden easily by changeVal function. (countdown1 and countdown2)
    - added jQuery into scripts for index.html. This is used to hide and show divs when choosing day option
    - reworked main.js so that the NodeJS Timer is inside of a function that won't run until variables are set and called by changeVal function.
    - myClock function housed inside clock function.

--- js/main.js
+++ js/main.js
@@ -1,73 +1,99 @@
-const currentDate = new Date();
+var h = 0
+var m = 0
+var umgap = 0
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const hour = urlParams.get("h") ?? currentDate.getHours() + 1;
-const minute = urlParams.get("m") ?? 0;
-const seconds = urlParams.get("s") ?? 0;
-const mGap = urlParams.get("mgap") ?? 60;
-const smGap = urlParams.get("smgap") ?? 1;
-
-const countDownDate = new Date(
-  currentDate.getFullYear(),
-  currentDate.getMonth(),
-  currentDate.getDate(),
-  hour,
-  minute,
-  seconds
-).getTime();
+function changeVal(choice) {
+  if (choice == 'sunday') {
+    h = 10
+    m = 0
+    umgap = 60
+    clock()
+    jQuery(function($) {
+      $("#firstpage").hide();
+      $("#countdown1").show();
+      $("#countdown2").show();
+    });
+  } else if (choice == 'thursday'){
+    h = 19
+    m = 30
+    umgap = 60
+    clock()
+    jQuery(function($) {
+      $("#firstpage").hide();
+      $("#countdown1").show();
+      $("#countdown2").show();
+    });
+    
+  }
+}
 
 //defining var notTime as false boolean
 var notTime = false;
 
-const x = setInterval(function() {
-  const now = new Date().getTime();
-  const distance = countDownDate - now;
 
-  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) + hours * 60;
-  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  const minuteSeconds = minutes + (100 / 60 * seconds) * 0.01;
+function clock() {
 
-  const isShort = minuteSeconds <= smGap;
+  const currentDate = new Date();
 
-  const selGap = isShort ? smGap : mGap;
-  const percentageRemaining = 360 - 100 / selGap * minuteSeconds * 0.01 * 360;
-  document.getElementById("countdown-circle").style.background = `
-    conic-gradient(#345996 ${percentageRemaining}deg, #f1f1f1 0%)
-    `;
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  var hour = parseInt(h) 
+  var minute = parseInt(m)
+  const seconds = urlParams.get("s") ?? 0;
+  var mGap = parseInt(umgap)
+  const smGap = urlParams.get("smgap") ?? 1;
 
-  const digitalCountdownElem = document.getElementById("digital-countdown");
+  var countDownDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    currentDate.getDate(),
+    hour,
+    minute,
+    seconds
+  ).getTime();
 
-  if (isShort) {
-    digitalCountdownElem.innerHTML = `${minutes * 60 + seconds}`;
- //alert("TEST ALERT isShort if statement ran");
+  const x = setInterval(myClock, 1000);
+    
+  function myClock() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
 
-   // document.getElementById("reminders-text").innerHTML = `
-   //   <p>The meeting is about to begin</p>
-   //   <p>Please remain muted until the meeting beings. You will need to mute and unmute yourself while commenting</p>
-   //   <p>If you choose to have your camera on, please be alert to maintain a modest appearance and background</p>
-   //   <p>Use the raise hand feature to participate</p>
-   //   `;
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) + hours * 60;
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const minuteSeconds = minutes + (100 / 60 * seconds) * 0.01;
 
-   //if statement that's not running for some reason???
-   if (notTime === false) {
-    document.getElementById("reminders-text").innerHTML = `
-    <div id="reminders-text" class="reminders-group">
-    <video autoplay muted playsinline>
-    <source src="final1.webm">
-    </div>
-    `
-    notTime = true;
-  };
-  } else {
-    const extraZero = seconds <= 9 ? "0" : ""
-    digitalCountdownElem.innerHTML = minutes + ":" + extraZero + seconds;
-    // setting var to false in else statement
-    notTime = false;
+    const isShort = minuteSeconds <= smGap;
+
+    const selGap = isShort ? smGap : mGap;
+    const percentageRemaining = 360 - 100 / selGap * minuteSeconds * 0.01 * 360;
+    document.getElementById("countdown-circle").style.background = `
+      conic-gradient(#345996 ${percentageRemaining}deg, #f1f1f1 0%)
+      `;
+
+    const digitalCountdownElem = document.getElementById("digital-countdown");
+
+    if (isShort) {
+      digitalCountdownElem.innerHTML = `${minutes * 60 + seconds}`;
+
+      if (notTime === false) {
+        document.getElementById("reminders-text").innerHTML = `
+        <div id="reminders-text" class="reminders-group">
+        <video autoplay muted playsinline>
+        <source src="final1.webm">
+        </div>
+        `
+        notTime = true;
+    };
+    } else {
+      const extraZero = seconds <= 9 ? "0" : ""
+      digitalCountdownElem.innerHTML = minutes + ":" + extraZero + seconds;
+      // setting var to false in else statement
+      notTime = false;
+    }
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("digital-countdown").innerHTML = "0";
+    }
   }
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("digital-countdown").innerHTML = "0";
-  }
-}, 1000);
+}
